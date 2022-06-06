ea3e2c8ec68585939ff43bcf97a873ebd410199e
Tim Gelinas <tgelinas@mreach.com>
2022-06-06 10:47:45 -0400

    initial commit/bootstrapping rails/react

---
+++ config/webpack/webpack.config.js
@@ -0,0 +1,17 @@
+// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
+
+const { webpackConfig, inliningCss } = require('shakapacker');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+if (isDevelopment && inliningCss) {
+  webpackConfig.plugins.push(
+    new ReactRefreshWebpackPlugin({
+      overlay: {
+        sockPort: webpackConfig.devServer.port,
+      },
+    })
+  );
+}
+
+module.exports = webpackConfig;
